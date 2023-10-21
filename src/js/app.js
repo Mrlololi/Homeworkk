9f0ad16b6cac15c479b8bd4edfb13780175256ff
mrlololi <ivikosha@gmail.com>
2023-10-22 01:10:00 +0400

    homework done

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,71 @@
-export let heroHealthColor;
+ export class Character {
+    constructor(name, type, health = 100, level = 1, attack, defence) {
+        if (name.length < 2 || name.length > 10) {
+            throw new Error('Имя должно быть от 2 до 10 символов');
+        }
+        this.name = name;
+        if (!['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'].includes(type)) {
+            throw new Error('Такого персонажа нет');
+        }        
+        this.type = type;
+        this.health = health;
+        this.level = level;
+        this.attack = attack;
+        this.defence = defence;
+    }
 
-export function showHealthColor(hero) {
-    if(hero.health > 50){
-        heroHealthColor = 'green';
-        return 'healthy';
-    } else if(hero.health <= 50 && hero.health >= 15){
-        heroHealthColor = 'yellow';
-        return 'wounded';
-    } else if(hero.health < 15){
-        heroHealthColor = 'red';
-        return 'critical';
+    levelUp() {
+        if(this.health <= 0) {
+            throw new Error('Нельзя повысить левел умершего');
+        }
+        this.level += 1;
+        this.attack = Math.round(this.attack * 1.2);
+        this.defence = Math.round(this.defence * 1.2);
+        this.health = 100;
+    }
+
+    damage(points) {
+        if (this.health >= 0){
+            this.health -= points * (1 - this.defence / 100);
+            if (this.health < 0) {
+                this.health = 0;
+            }
+        }
     }
 }
 
-export function sortByHealth(heroList){
-    return heroList.sort((a, b) => a.health - b.health);
+export class Bowman extends Character {
+    constructor(name) {
+        super(name, 'Bowman', 100, 1, 25, 25);
+    }
+}
+
+export class Swordsman extends Character {
+    constructor(name) {
+        super(name, 'Swordsman', 100, 1, 40, 10);
+    }
+}
+
+export class Magician extends Character {
+    constructor(name) {
+        super(name, 'Magician', 100, 1, 10, 40);
+    }
+}
+
+export class Daemon extends Character {
+    constructor(name) {
+        super(name, 'Daemon', 100, 1, 25, 25);
+    }
+}
+
+export class Undead extends Character {
+    constructor(name) {
+        super(name, 'Undead', 100, 1, 40, 10);
+    }
+}
+
+export class Zombie extends Character {
+    constructor(name) {
+        super(name, 'Zombie', 100, 1, 10, 40);
+    }
 }
--- src/js/app.test.js
+++ src/js/app.test.js
@@ -1,24 +1,50 @@
-import { showHealthColor, heroHealthColor, sortByHealth } from './app.js';
+import { Character, Bowman, Swordsman, Magician, Daemon, Undead, Zombie } from "./app.js";
 
-
-test('showHealthColor should return healthy for health > 50', () => {
-    const result = showHealthColor({ health: 90 });
-    expect(result).toBe('healthy');
-});
-  
-test('showHealthColor should return wounded for 15 <= health <= 50', () => {
-    const result = showHealthColor({ health: 20 });
-    expect(result).toBe('wounded');
-});
-  
-test('showHealthColor should return critical for health < 15', () => {
-    const result = showHealthColor({ health: 10 });
-    expect(result).toBe('critical');
+test("should throw error for short name", () => {
+    expect(() => {
+        new Character("A", "Bowman", 100, 1, 25, 25);
+    }).toThrow("Имя должно быть от 2 до 10 символов");
 });
 
-test('sortByHealth should return sorted array', () => {
-    const result = sortByHealth([{ name: 'мечник', health: 10 }, { name: 'маг', health: 100 }, { name: 'лучник', health: 80 }]);
-    expect(result).toEqual([{ name: 'мечник', health: 10 }, { name: 'лучник', health: 80 }, { name: 'маг', health: 100 }]);
-})  
+test("should throw error for unknown type", () => {
+    expect(() => {
+        new Character("Archer", "Unknown", 100, 1, 25, 25);
+    }).toThrow("Такого персонажа нет");
+});
 
-console.log(heroHealthColor);
+test("should level up correctly", () => {
+    const char = new Character("Archer", "Bowman", 100, 1, 25, 25);
+    char.levelUp();
+    expect(char.level).toBe(2);
+    expect(char.attack).toBe(30);
+    expect(char.defence).toBe(30);
+    expect(char.health).toBe(100);
+});
+
+test("should throw error when leveling up a dead character", () => {
+    const char = new Character("Archer", "Bowman", 0, 1, 25, 25);
+    expect(() => {
+        char.levelUp();
+    }).toThrow("Нельзя повысить левел умершего");
+});
+
+test("should apply damage correctly", () => {
+    const char = new Character("Archer", "Bowman", 100, 1, 25, 25);
+    char.damage(50);
+    expect(char.health).toBeLessThan(100);
+}); 
+
+test("should not allow health to go below 0", () => {
+    const char = new Character("Archer", "Bowman", 20, 1, 25, 25);
+    char.damage(50);
+    expect(char.health).toBe(0);
+});
+
+test("should level up a character with partial health", () => {
+    const char = new Character("Archer", "Bowman", 50, 1, 25, 25);
+    char.levelUp();
+    expect(char.level).toBe(2);
+    expect(char.attack).toBe(30);
+    expect(char.defence).toBe(30);
+    expect(char.health).toBe(100);
+});
