b543283ca8f31ad97391d94567eedbbd8e261314
Iva Tsatskrialashvili <ivikosha@gmail.com>
2023-11-04 01:35:46 +0400

    task fixed

--- src/js/app.js
+++ src/js/app.js
@@ -1,61 +1,56 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-empty */
-// const obj = {name: 'мечник', health: 10, level: 2, attack: 80, defence: 40}
+export const obj = { name: 'мечник', health: 10, level: 2, attack: 80, defence: 40 };
 
-// function orderByProps(object, firstSort) {
-//     const orderedProps = [];
-//     const remainingProps = [];
+export function orderByProps(object, firstSort) {
+  const orderedProps = [];
+  const remainingProps = [];
 
-//     for (let key in object) {
-//         if (firstSort.includes(key)) {
-//             orderedProps.push({ key: key, value: object[key] });
-//         } else {
-//             remainingProps.push({ key: key, value: object[key] });
-//         }
-//     }
-
-//     const remainingPropsSorted = remainingProps.sort((a, b) => a.key.localeCompare(b.key));
-
-//     const newObj = orderedProps.concat(remainingPropsSorted);
-
-//     return newObj;
-// }
-
-// const newObj = orderByProps(obj, ["name", "level"]);
-
-const character = {
-    name: 'Лучник',
-    type: 'Bowman',
-    health: 50,
-    level: 3,
-    attack: 40,
-    defence: 10,
-    special: [
-      {
-        id: 8,
-        name: 'Двойной выстрел',
-        icon: 'http://...',
-        description: 'Двойной выстрел наносит двойной урон'
-      }, 
-      {
-        id: 9,
-        name: 'Нокаутирующий удар',
-        icon: 'http://...',
-      }
-    ]	
-  }
-
-let moves = []
-
-  function attackWindow(player) {
-    player.special.forEach((specialMove, index) => {
-        if(!specialMove.description){
-            specialMove.description = "Описание недоступно"
+  for (const key in object) {
+    if (firstSort.includes(key)) {
+      orderedProps.push({ key: key, value: object[key] });
+    } else {
+      remainingProps.push({ key: key, value: object[key] });
     }
-    moves.push(specialMove);
-    });
   }
 
-attackWindow(character)
+  const remainingPropsSorted = remainingProps.sort((a, b) => a.key.localeCompare(b.key));
+  const newObj = orderedProps.concat(remainingPropsSorted);
 
-console.log(moves)
+  return newObj;
+}
+
+export const character = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон'
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...'
+    }
+  ]
+};
+
+export function attackWindow(player) {
+  const processedMoves = player.special.map((specialMove) => ({
+    ...specialMove,
+    description: specialMove.description || "Описание недоступно",
+  }));
+
+  return processedMoves;
+}
+
+const sortedProps = orderByProps(obj, ["name", "level"]);
+console.log(sortedProps);
+
+const moves = attackWindow(character);
+console.log(moves);
--- src/js/app.test.js
+++ src/js/app.test.js
@@ -1,2 +1,65 @@
-/* eslint-disable no-unused-vars */
-import { Character } from "./app.js";
+import { obj, orderByProps, character, attackWindow, moves } from './app.js';
+
+describe('orderByProps function', () => {
+  it('should return an array with sorted properties by the given order and the rest in alphabetical order', () => {
+    const expected = [
+      { key: 'name', value: 'мечник' },
+      { key: 'level', value: 2 },
+      { key: 'attack', value: 80 },
+      { key: 'defence', value: 40 },
+      { key: 'health', value: 10 },
+    ];
+    expect(orderByProps(obj, ['name', 'level'])).toEqual(expected);
+  });
+
+  it('should sort unspecified properties in alphabetical order', () => {
+    const expected = [
+      { key: 'name', value: 'мечник' },
+      { key: 'attack', value: 80 },
+      { key: 'defence', value: 40 },
+      { key: 'health', value: 10 },
+      { key: 'level', value: 2 },
+    ];
+    expect(orderByProps(obj, ['name'])).toEqual(expected);
+  });
+
+  it('should return an empty array if the object is empty', () => {
+    expect(orderByProps({}, ['name', 'level'])).toEqual([]);
+  });
+});
+
+describe('attackWindow function', () => {
+  it('should add a default description to moves without one', () => {
+    attackWindow(character);
+    expect(character.special[1].description).toBe('Описание недоступно');
+  });
+
+  it('should not overwrite existing descriptions', () => {
+    attackWindow(character);
+    expect(character.special[0].description).toBe('Двойной выстрел наносит двойной урон');
+  });
+
+  it('should populate the moves array with special moves from the character', () => {
+    const expected = [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон'
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+        description: 'Описание недоступно'
+      }
+    ];
+    attackWindow(character);
+    expect(moves).toEqual(expected);
+  });
+
+  it('should return the updated moves array', () => {
+    const result = attackWindow(character);
+    expect(result).toEqual(moves);
+  });
+});
